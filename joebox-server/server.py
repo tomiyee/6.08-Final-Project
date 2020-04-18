57331e2e5c152db7e92cf8607073aca28bc3724d
tomiyee <tommy.and.heng@gmail.com>
2020-04-18 12:34:02 -0400

    fixed importing handlers with correct dir

--- joebox-server/server.py
+++ joebox-server/server.py
@@ -2,19 +2,25 @@ import numpy as np
 import time
 import datetime
 import sqlite3
+import json
+import sys
+sys.path.append('__HOME__/bluffalo')
 # import individual action handlers
 from game_check import in_lobby, room_code_check
 from word_prompt import current_prompt
 from join_room import join_room
 from start_game import start_game
+from submit_bluff import submit_bluff
 
 # The database with game data for everyone
 bluffalo_db = '__HOME__/bluffalo/game_data.db'
 
 def request_handler (request) :
     if request['method'] == 'GET':
+        if 'action' not in request['args']:
+            return "Error: All requests need an 'action.'"
         # Determines which action the http request wants done
-        action = request['value']['action']
+        action = request['values']['action']
         # Selects the correct handler for the action
         if action == "in_lobby":
             return in_lobby(request)
@@ -24,6 +30,8 @@ def request_handler (request) :
             return current_prompt(request)
 
     if request['method'] == 'POST':
+        if 'action' not in request['form']:
+            return "Error: All requests need an 'action.'"
         # Determines which action the http request wants done
         action = request['form']['action']
         # Selects the correct handler for the action
@@ -31,6 +39,10 @@ def request_handler (request) :
             return join_room(request)
         if action == "start_game":
             return start_game(request)
+        if action == "submit_bluff":
+            return submit_bluff(request)
+
+    return "Unhandled action: " + action + ". Double check the action is spelled correctly."
 
 def initialize_game_db ():
     """
