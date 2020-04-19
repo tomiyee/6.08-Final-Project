4dcceeb6a97c87856f3bc66a5c0928f091b5f6e7
kgao2000 <59459414+kgao2000@users.noreply.github.com>
2020-04-18 22:39:14 -0400

    have not tested yet

--- joebox-server/submit_bluff.py
+++ joebox-server/submit_bluff.py
@@ -13,21 +13,39 @@ def submit_bluff (request):
 
     Returns String "Success" if successfully added, String "None" if failed to add
     """
-
-    # room_json = Fetch the JSON String from the SQL with the right room code
-    # room = json.load(room_json)
-    # Set room[“player_data”][user].submitted = True
-    # Set room[“player_data”][user].submission = bluff
-
-    # If all players.submitted = True
-    #   Set room[“game_data”].waiting_for_submissions = False
-    #   Set room[“game_data”].selecting_options to True
-
-    # Update SQL with updated json room data
-    # new_room_json = json.dumps(room)
-
-    # json load: turns json file into python dictionary
-    # json dump: turns dictionary to json thing
-
-
-    pass
+    conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
+    connection = conn.cursor() 
+    
+    room_code = request['form']['roomcode']
+    user = request['form']['user_name'] 
+    bluff_submitted = request['form']['bluff'] # text submission user entered on ESP
+    
+    connection.execute('''CREATE TABLE IF NOT EXISTS game_table (roomcode text, Player_and_Room_data text);''')
+    try: 
+        room_json = connection.execute('''SELECT * FROM game_table WHERE roomcode = ?;''', (room_code,))
+        # Fetch the JSON String from the SQL with the right room code
+        
+        # json load: turns json file into python dictionary
+        room = json.load(room_json)
+        room['player_data'][user].submitted = True
+        room['player_data'][user].submission = bluff_submitted
+        
+        all_players_submitted = True
+        for players in room['player_data']: 
+            if player['submitted'] == False: 
+                all_players_submitted = False
+                break
+            
+        if all_players_submitted: #if all players submitted
+            room['game_data']['waiting_for_submissions'] = False
+            room['game_data']['selecting_options'] = True   
+    
+        # json dump: turns dictionary to json thing
+        new_room_json = json.dumps(room)
+        
+        #update SQL with updated json room data
+        connection.execute('''UPDATE game_table SET Player_and_Room_data =? WHERE roomcode =?;''', (new_room_json, room_code))
+        return 'Success' #if successfully added
+    except: 
+        return 'None'
+    
