af2a21e9ab48ad6d0df6bd234baa1a8d31d495c7
kgao2000 <59459414+kgao2000@users.noreply.github.com>
2020-04-19 14:22:56 -0400

    added more error codes

--- joebox-server/join_room.py
+++ joebox-server/join_room.py
@@ -11,7 +11,7 @@ def join_room (request):
       String user      - The name of the player entered on ESP
 
     Returns a string, representing all usernames separated by ","
-    Error codes: 
+    Error codes:
     -1: unknown error
     4: username already exists
     5: game not in lobby: can't join right now
@@ -22,79 +22,77 @@ def join_room (request):
     """
 
     conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
-    connection = conn.cursor() 
-    
+    connection = conn.cursor()
+
     #data from request: room code, player user name, and their bluff submitted
-    try: 
+    try:
         room_code = request['form']['roomcode']
-        user = request['form']['username'] 
-    except: 
+        user = request['form']['username']
+    except:
         conn.commit()
         conn.close()
-        return '9' #one of the parameters in POST missing 
-    
+        return '9' #one of the parameters in POST missing
+
     try:
         rooms_json = connection.execute('''SELECT game_data FROM game_table WHERE room_code = ?;''', (room_code,))[0][0]
         # Fetch the JSON String from the SQL with the right room code
-        if len(rooms_json) == 0: 
+        if len(rooms_json) == 0:
             conn.commit() # commit commands
             conn.close() # close connection to database
             return '6' #room does not exist yet
-        
-        if len(rooms_json) > 1: 
+
+        if len(rooms_json) > 1:
             conn.commit() # commit commands
             conn.close() # close connection to database
             return '7' #there are more than one room with this room code
         # json load: turns json file into python dictionary
         room = json.load(rooms_json[0][0])
-        
-        if user in room['player_data']: 
+
+        if user in room['player_data']:
             conn.commit() # commit commands
             conn.close() # close connection to database
-            return '4' #can't join room under the same username, username already exists 
-        
-        if room['game_data']['in_lobby']: 
+            return '4' #can't join room under the same username, username already exists
+
+        if room['game_data']['in_lobby']:
             conn.commit() # commit commands
             conn.close() # close connection to database
             return '5' #can't join room while game is not in lobby
-        
+
         room['player_data'][user]["score"] = 0
         room['player_data'][user]["submitted"] = False
         room['player_data'][user]["submission"] = None
-        
+
         # json dump: turns dictionary to json thing
         new_room_json = json.dumps(room)
-            
+
         #update SQL with updated json room data
         connection.execute('''UPDATE game_table SET game_data =? WHERE room_code =?;''', (new_room_json, room_code))
-        
+
         players = [player for player in room['player_data']]
-        
+
         out = ''
-        for player in players: 
+        for player in players:
             out += player
             out += ','
-    
+
         conn.commit() # commit commands
         conn.close() # close connection to database
-        
+
         return out #string representing all usernames separated by ","
         #example: jimmy,karen,michelle,
-    except: 
+    except:
         conn.commit() # commit commands
         conn.close() # close connection to database
-        
+
         return '-1' #don't know what happened
 
 #players = ['karen', 'michelle']
 #out = ''
-#for player in players: 
+#for player in players:
 #    out += player
 #    out += ','
 #print(out)
-    
+
 #print(len([[1],[2]]))
 #lst = [[1], [2]]
 #print(lst[0][0])
-    
-
