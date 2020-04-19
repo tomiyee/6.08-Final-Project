ab0bff405a4d75df6f891abb7896a1a1f5f11906
kgao2000 <59459414+kgao2000@users.noreply.github.com>
2020-04-19 14:11:51 -0400

    removed unecessary comments

--- joebox-server/submit_bluff.py
+++ joebox-server/submit_bluff.py
@@ -11,7 +11,7 @@ def submit_bluff (request):
       String user      - The name of the player entered on ESP
       String bluff     - The text submission that the user entered on the ESP
 
-    Returns an error code if failed to add: 
+    Returns an error code if failed to add:
     -1: unknown error, don't know what happened
     1: if they already submitted a bluff this round
     2: game in lobby, can't submit bluffs right now
@@ -22,72 +22,70 @@ def submit_bluff (request):
     see number of people who haven't submitted a bluff yet in Postman
     """
     conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
-    connection = conn.cursor() 
-    
+    connection = conn.cursor()
+
     #data from request: room code, player user name, and their bluff submitted
-    
-    try: 
+
+    try:
         room_code = request['form']['roomcode']
-        user = request['form']['user_name'] 
+        user = request['form']['user_name']
         bluff_submitted = request['form']['bluff'] # text submission user entered on ESP
-    except: 
+    except:
         conn.commit() # commit commands
         conn.close() # close connection to database
         return '9' #one of the required parameters are missing
-    
-#    connection.execute('''CREATE TABLE IF NOT EXISTS game_table (roomcode text, current_games text);''')
+
     try: 
         room_json = connection.execute('''SELECT game_data FROM game_table WHERE room_code = ?;''', (room_code,))[0][0]
         # Fetch the JSON String from the SQL with the right room code
-        
+
         # json load: turns json file into python dictionary
         room = json.load(room_json)
-        
-        if user not in room['player_data']: 
+
+        if user not in room['player_data']:
             return '8' #player doesn't exist in game
         if room['player_data'][user]["submitted"]:
             conn.commit() # commit commands
             conn.close() # close connection to database
             return '1'  #Player already submitted a bluff this round
-        
-        if room['game_data']['in_lobby']: 
+
+        if room['game_data']['in_lobby']:
             conn.commit() # commit commands
             conn.close() # close connection to database
             return '2' #in lobby right now, can't submit bluffs
-        
-        if not room['game_data']['waiting_for_submissions']: 
+
+        if not room['game_data']['waiting_for_submissions']:
             conn.commit() # commit commands
             conn.close() # close connection to database
             return '3' #not waiting for submissions, can't submit bluffs right now
-        
-        
+
+
         room['player_data'][user]["submitted"] = True
         room['player_data'][user]["submission"] = bluff_submitted
-        
+
         all_players_submitted = True
         num_no_submission = 0 #players who haven't submitted yet
-        for player in room['player_data']: 
-            if not player['submitted']: 
+        for player in room['player_data']:
+            if not player['submitted']:
                 all_players_submitted = False
                 num_no_submission += 1
-        
+
         if all_players_submitted: #if all players submitted
             room['game_data']['waiting_for_submissions'] = False
-            room['game_data']['selecting_options'] = True   
-    
+            room['game_data']['selecting_options'] = True
+
         # json dump: turns dictionary to json thing
         new_room_json = json.dumps(room)
-        
+
         #update SQL with updated json room data
         connection.execute('''UPDATE game_table SET game_data =? WHERE room_code =?;''', (new_room_json, room_code))
         conn.commit() # commit commands
         conn.close() # close connection to database
         return num_no_submission #if successfully added
-    
+
     except: #if not successfully added
         conn.commit() # commit commands
         conn.close() # close connection to database
         return '-1' #don't know what happened
 
     #test using post request in postman: put all content under post body
-    
