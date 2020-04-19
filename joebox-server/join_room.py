fca3e097801c6e62de75c3993623daef8d5e865a
kgao2000 <59459414+kgao2000@users.noreply.github.com>
2020-04-18 22:57:51 -0400

    added join_room.py not tested yet

--- joebox-server/join_room.py
+++ joebox-server/join_room.py
@@ -13,4 +13,34 @@ def join_room (request):
     Returns a string, representing all usernames separated by ","
     """
 
-    pass
+    conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
+    connection = conn.cursor() 
+    
+    #data from request: room code, player user name, and their bluff submitted
+    room_code = request['form']['roomcode']
+    user = request['form']['username'] 
+    
+    connection.execute('''CREATE TABLE IF NOT EXISTS game_table (roomcode text, Player_and_Room_data text);''')
+
+    room_json = connection.execute('''SELECT * FROM game_table WHERE roomcode = ?;''', (room_code,))
+    # Fetch the JSON String from the SQL with the right room code
+    
+    # json load: turns json file into python dictionary
+    room = json.load(room_json)
+    room['player_data'][user]["score"] = 0
+    room['player_data'][user]["submitted"] = False
+    room['player_data'][user]["submission"] = None
+    
+    # json dump: turns dictionary to json thing
+    new_room_json = json.dumps(room)
+        
+    #update SQL with updated json room data
+    connection.execute('''UPDATE game_table SET Player_and_Room_data =? WHERE roomcode =?;''', (new_room_json, room_code))
+    
+    players = [player for player in room['player_data']]
+    
+    return str(players) #string representing all usernames separated by ","
+
+  
+    
+
