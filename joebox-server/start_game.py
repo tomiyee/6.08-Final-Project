110816b8bd7907e850a3c5aec85d7b6769abc3f9
tomiyee <tommy.and.heng@gmail.com>
2020-04-25 17:01:27 -0400

    Creates List of Prompts for the Game at Start

--- joebox-server/start_game.py
+++ joebox-server/start_game.py
@@ -1,8 +1,11 @@
 import json
 import sqlite3
 import os
+import random
+
 
 bluffalo_db = os.path.dirname(__file__) + '/game_data.db'
+word_db = os.path.dirname(__file__) + '/../word-database/word_bank.db'
 # Note json.loads(String) and json.dumps(Objects)
 
 def start_game (request):
@@ -38,11 +41,19 @@ def start_game (request):
 
     # (TODO) Trigger events that occur once the game starts, like setting the question
 
+    conn2 = sqlite3.connect(word_db)  # connect to that database (will create if it doesn't already exist)
+    connection2 = conn2.cursor()
+    num_words = connection2.execute('''SELECT COUNT(*) FROM word_table;''').fetchone()[0]
+    word_indices = random.sample(range(num_words), 7)
+    words = [connection2.execute('''SELECT * FROM word_table LIMIT 1 OFFSET ?;''', (i,)).fetchone() for i in word_indices]
+    room_data['game_data']['all_prompts'] = words
+    conn2.commit()
+    conn2.close()
 
     new_room_json = json.dumps(room_data)
     connection.execute('''UPDATE game_table SET game_data =? WHERE room_code =?;''', (new_room_json, room_code))
-    conn.commit() # commit commands
-    conn.close() # close connection to database
+    conn.commit()
+    conn.close()
 
     # Constructs a list of player names separated by commas
     players = ",".join([player for player in room_data['player_data']])
