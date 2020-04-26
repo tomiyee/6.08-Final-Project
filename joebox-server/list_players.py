442e1713f072b97c661583a32a68dd412bbbc160
kgao2000 <59459414+kgao2000@users.noreply.github.com>
2020-04-26 18:34:05 -0400

    draft one untested

--- joebox-server/list_players.py
+++ joebox-server/list_players.py
@@ -15,5 +15,30 @@ def list_players (request):
 
     players = []
 
+    if 'room_code' not in request['values']:
+        return "Missing Room Code"
+    room_code = request['values']['room_code']
+
+
+    conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
+    connection = conn.cursor()  # move cursor into database (allows us to execute commands)
+    room_rows = connection.execute('''SELECT game_data FROM game_table WHERE room_code = ?;''', (room_code,)).fetchall()
+    conn.commit()
+    conn.close()
+
+    if len(room_rows) == 0:
+        return "No room with room code: " + room_code
+
+    if len(room_roms) > 1:
+        return "More than one room with this room code"
+
+    #dictionary of game and player data for room with given room code
+    room_data = json.loads(room_rows[0][0])
+    #list of players currently in game
+    players = [player for player in room_data['player_data']:
+
+    if len(players) == 0:
+        return "No players in game currently"
+
     # Joins the list of strings with commas in between.
     return ",".join(players)
