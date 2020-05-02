9f09b12b7b2efca6723c8bd3dd56c73d866de8ac
kgao2000 <59459414+kgao2000@users.noreply.github.com>
2020-05-01 23:34:01 -0400

    added new function for get request

---
+++ joebox-server/score_rank.py
@@ -0,0 +1,46 @@
+import json
+import sqlite3
+import os
+
+bluffalo_db = os.path.dirname(__file__) + '/game_data.db'
+# Note json.loads(String) and json.dumps(Objects)
+
+def score_rank (request):
+    """
+    Given the GET request with:
+      String room_code - The characters that rep the room code
+
+    Returns a comma-separated string of players along with their current get_scores
+    sorted from highest to lowerst score in the specified room
+    """
+
+    #dictionary of player name and their current score
+    scores = {}
+
+    if 'room_code' not in request['values']:
+        return "Missing Room Code"
+    room_code = request['values']['room_code']
+
+
+    conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
+    connection = conn.cursor()  # move cursor into database (allows us to execute commands)
+    room_rows = connection.execute('''SELECT game_data FROM game_table WHERE room_code = ?;''', (room_code,)).fetchall()
+    conn.commit()
+    conn.close()
+
+    if len(room_rows) == 0:
+        return "No room with room code: " + room_code
+
+    if len(room_rows) > 1:
+        return "More than one room with this room code"
+
+    #dictionary of game and player data for room with given room code
+    room_data = json.loads(room_rows[0][0])
+    #list of players currently in game
+    players = [player for player in room_data['player_data']]
+
+    if len(players) == 0:
+        return "No players in game currently"
+
+    # Joins the list of strings with commas in between.
+    return ",".join(players)
