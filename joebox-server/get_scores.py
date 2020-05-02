d20ee8abc0b74eb72e119ff6accbba2b3735cedd
tomiyee <tommy.and.heng@gmail.com>
2020-05-02 01:32:55 -0700

    Implement Get Scores

--- joebox-server/get_scores.py
+++ joebox-server/get_scores.py
@@ -21,5 +21,30 @@ def get_scores (request):
     Returns a comma-separated string of players and their scores
     """
 
-    # Temporary Return Value
-    return "Player1,100,Player2,200,Player3,150"
+    if 'room_code' not in request['values']:
+        return "Misssing Room Code"
+    room_code = request['values']['room_code']
+
+
+    conn = sqlite3.connect(bluffalo_db)  # connect to that database (will create if it doesn't already exist)
+    connection = conn.cursor()  # move cursor into database (allows us to execute commands)
+    room_rows = connection.execute('''SELECT game_data FROM game_table WHERE room_code = ?;''', (room_code,)).fetchall()
+    conn.commit()
+    conn.close()
+
+    if len(room_rows) == 0:
+        return "No room with room code: " + room_code
+
+    if len(room_rows) > 1:
+        return "More than one room with this room code"
+
+    #dictionary of game and player data for room with given room code
+    room_data = json.loads(room_rows[0][0])
+    #list of players currently in game
+    data = []
+    for player in room_data['player_data']:
+        data.append(player)
+        data.append(str(room_data['player_data'][player]['score']))
+
+    # Joins the list of strings with commas in between.
+    return ",".join(data)
