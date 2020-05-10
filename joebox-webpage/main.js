519de25549e608ed922c48c5b5fdacb0cfc58889
tomiyee <tommy.and.heng@gmail.com>
2020-05-10 00:21:30 -0700

    Added Correct Ans and End of Game Handler

--- joebox-webpage/main.js
+++ joebox-webpage/main.js
@@ -9,6 +9,8 @@ let roomCodeInput;
 let roomCode;
 let roomData;
 let animating = false;
+let intervalId;
+
 // Makes sure that start() is called right away
 window.onload = start;
 
@@ -22,14 +24,12 @@ function start () {
   roomCodeInput = $("#room-code-input");
   $('#room-code-input').keypress(inputKeyPressHandler);
   $('#room-code-input').keydown(inputKeyDownHandler);
-  $('.game-container').hide();
-  $('.lobby-container').hide();
-  $('.error-room').hide();
+  hideAllOthers('.input-container');
   $('.button-room-input').click(inputButtonPressHandler);
   $('.button-start-game').click(startGameButtonPressHandler);
 
   // Sets the loop to be called every DT ms
-  setInterval (loop, DT);
+  intervalId = setInterval (loop, DT);
 }
 
 /**
@@ -43,53 +43,72 @@ async function loop () {
   // 1. Send a request to the server for game data
   let resp = await sendHttpRequest("GET", SERVER_URL+"?action=dump_data&room_code=" + roomCode);
   let newRoomData = JSON.parse(resp);
-  if (!roomData) {
-    roomData = newRoomData;
-  }
+  // For the initial fetch from the server
+  if (!roomData) roomData = newRoomData;
 
-  // Going from voting to submitting
-  if (!newRoomData['game_data']['waiting_for_votes'] && roomData['game_data']['waiting_for_votes']) {
+  // 2. Check for Transitions to Animate
+  if (!newRoomData['game_data']['waiting_for_votes'] && roomData['game_data']['waiting_for_votes'])
     onVoteEndHandler(roomData, newRoomData);
-   }
+  if (newRoomData['game_data'].round_number > roomData['game_data'].round_number)
+    onRoundEndHandler(roomData, newRoomData);
+  if (newRoomData['game_data']['in_lobby'] && !roomData['game_data']['in_lobby'])
+    onGameEndHandler(roomData, newRoomData);
 
-   // if (newRoomData['game_data']['waiting_for_votes'] && roomData['game_data']['waiting_for_votes']) {
-   //   onVoteEndHandler();
-   //  }
-  roomData = JSON.parse(resp);
+  roomData = newRoomData;
 
-  // 2a. If in lobby, update the list of players
+  // 3a. If in lobby, update the list of players
   if (!animating && roomData['game_data']['in_lobby'] && roomData['game_data']['round_number'] != 3) {
     hideAllOthers('.lobby-container');
     displayLobby();
   }
 
-  // 2b If waiting for submissions, update list of waiting.
+  // 3b. If waiting for submissions, update list of waiting.
   if (!animating && (roomData['game_data']['waiting_for_submissions'] ||
       roomData['game_data']['waiting_for_votes'])) {
     hideAllOthers('.game-container');
     displayPrompt();
-      displayOptions();
+    displayPlayers();
+    displayOptions();
     if (roomData['game_data']['waiting_for_votes'])
       $('.options-container').show();
     else
       $('.options-container').hide();
-    displayPlayers();
   }
 }
 
+/**
+ * @function onVoteEndHandler - Triggers animations that happen after everyone
+ * has voted, like showing how the rank has changed
+ *
+ * @param  {type} old  The old game data dictionary
+ * @param  {type} _new The new game data dictionary
+ */
 function onVoteEndHandler (old, _new) {
-  //Start animation
-
+  console.log(`onVoteEndHandler(${JSON.stringify(old)}, ${JSON.stringify(_new)})`);
+  // Shows the container
   hideAllOthers(".score-container");
   $(".scoreboard").empty();
 
   let oldSortedScores = [];
   let newSortedScores = [];
-  // Animations
+
+  // Adds a row for the Correct Answer
+  let r = old['game_data']['round_number'];
+  let q = old['game_data']['question_number'];
+  let ans = old['game_data']['all_prompts'][(r-1)*3+q-1][2];
+  let ans_row = $(document.createElement('tr'))
+    .append($(document.createElement('td')).text('ANSWER'))
+    .append($(document.createElement('td')).text(ans))
+    .append($(document.createElement('td')).text('+' + r*1000))
+    .addClass("correct-answer")
+    .addClass('scoreboard-row');
+  $(".scoreboard").append(ans_row);
+  // Adds a row for each of the other players
   for(let p in old['player_data']) {
     // If the player's nametag is not yet displayed, then create it
     let row = $(document.createElement('tr'));
     let name = $(document.createElement('td'));
+    let bluff = $(document.createElement('td'));
     let score = $(document.createElement('td'));
 
     row.addClass('scoreboard-row');
@@ -97,12 +116,14 @@ function onVoteEndHandler (old, _new) {
     score.addClass('scoreboard-score-'+p);
     // Make the name and score elements a child of the row element
     row.append(name);
+    row.append(bluff);
     row.append(score);
     // Append data to the lists we will sort
     oldSortedScores.push([p, old['player_data'][p]['score']]);
     newSortedScores.push([p, _new['player_data'][p]['score']]);
-
+    // Updates the contents
     name.text(p);
+    bluff.text(old['player_data'][p]['submission'])
     score.text(old['player_data'][p]['score']);
 
     $(".scoreboard").append(row);
@@ -110,26 +131,25 @@ function onVoteEndHandler (old, _new) {
 
   oldSortedScores.sort(function(a,b) {return b[1]<a[1]?-1:1;});
   newSortedScores.sort(function(a,b) {return b[1]<a[1]?-1:1;});
-  let ROW_HEIGHT = 32;
+
+  let ROW_HEIGHT = 40;
   animating = true;
   //Show old scores briefly for 2 seconds
   for(let i in oldSortedScores) {
     let playerName = oldSortedScores[i][0];
     let row = $('.scoreboard-row-' + playerName);
-    row.css('top',i*ROW_HEIGHT+'px');
+    row.css('top', i * ROW_HEIGHT + ROW_HEIGHT + 10 + 'px');
   }
 
-  //Animate for 2 seconds
+  // Start the 2 second animation 2 seconds after the beginning
   setTimeout(function () {
     for(let i in newSortedScores) {
       let playerName = newSortedScores[i][0];
       let row = $('.scoreboard-row-' + playerName);
-      row.css('top',i*ROW_HEIGHT+'px');
-      $('.scoreboard-score-'+playerName).text(newSortedScores[i][1]);
+      row.css('top', i * ROW_HEIGHT + ROW_HEIGHT + 10 + 'px');
+      $('.scoreboard-score-' + playerName).text(newSortedScores[i][1]);
     }
   }, 2000);
-
-
 
   // When we want to HIDE the score animation
   setTimeout(function () {
@@ -137,8 +157,39 @@ function onVoteEndHandler (old, _new) {
   }, 6000);
 }
 
-function onBluffEndHandler (e) {
+/**
+ * @function onRoundEndHandler - Triggers animations that happen after a round
+ * ends, like displaying the announcement of a new round
+ *
+ * @param  {type} old  The old game data dictionary
+ * @param  {type} _new The new game data dictionary
+ */
+function onRoundEndHandler (old, _new) {
+  setTimeout(() => {
+    animating = true;
+    // Insert some animation
 
+    // ...
+  }, 6000);
+
+  // Finished Animations
+  setTimeout(() => {
+    animating = false;
+  }, 7000); // TODO add the duration of the animation
+}
+
+/**
+ * @function onGameEndHandler - Triggers animations that happen when the game
+ * ends like displaying the rankings
+ *
+ * @param  {type} old  The old game data dictionary
+ * @param  {type} _new The new game data dictionary
+ */
+function onGameEndHandler (old, _new) {
+  setTimeout(() => {
+    hideAllOthers(".score-container");
+    animating = true;
+  }, 6100);
 }
 
 /**
@@ -325,7 +376,6 @@ function displayPlayers () {
   }
 }
 
-
 function hideAllOthers (container) {
     $('.game-container').hide();
     $('.lobby-container').hide();
@@ -334,10 +384,6 @@ function hideAllOthers (container) {
     $(".score-container").hide();
 
     $(container).show();
-}
-
-function animateSumbittedBluffsPlayers () {
-  submittedPlayers = roomData['']
 }
 
 /**
@@ -349,7 +395,6 @@ function dump () {
     "https://608dev-2.net/sandbox/sc/team033/bluffalo/server.py?action=dump_data")
     .then((r) => console.log(r));
 }
-
 
 /**
  * @function sendHttpRequest - Handles sending HTTP Requests and wraps the process
