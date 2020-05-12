4d6299fc8dba0061204314a396f0572ea4a61554
tomiyee <tommy.and.heng@gmail.com>
2020-05-12 01:58:20 -0400

    Added Smart Gyro Enter

---
+++ joebox-esp/TextSlider.h
@@ -0,0 +1,18 @@
+#ifndef TextSlider_h
+#define TextSlider_h
+#include "Arduino.h"
+
+class TextSlider {
+    char alphabet[50] = " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char query_string[50] = {0};
+    int char_index;
+    int state;
+    unsigned long scrolling_timer;
+    const int scrolling_threshold = 150;
+    const float angle_threshold = 0.3;
+  public:
+    TextSlider ();
+    void update (float angle, int a_button, int b_button, char* output);
+    void getValue (char* output);
+};
+#endif
